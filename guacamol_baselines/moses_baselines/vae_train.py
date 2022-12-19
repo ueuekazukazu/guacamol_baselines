a8b087c968f93fb876efcf2db441b7f203b4f6f3
Jannis Born <jannis.born@gmx.de>
2022-12-19 14:48:06 +0100

    Model warmstarting (#3)
    * chore: mac files
    
    * feat: enable support for model finetuning
    
    * wip: update moses deps

--- guacamol_baselines/moses_baselines/vae_train.py
+++ guacamol_baselines/moses_baselines/vae_train.py
@@ -1,5 +1,7 @@
 # Adapted from https://github.com/molecularsets/moses/blob/master/scripts/vae/train.py
-
+from copy import deepcopy
+import logging
+import os
 import torch
 
 from moses.script_utils import add_train_args, set_seed
@@ -9,6 +11,8 @@ from moses.vae.model import VAE
 from moses.vae.trainer import VAETrainer
 
 from .common import read_smiles
+
+logger = logging.getLogger(__name__)
 
 
 def get_parser():
@@ -29,12 +33,33 @@ def main(config):
     corpus = OneHotCorpus(config.n_batch, device)
     train = corpus.fit(train).transform(train)
 
-    model = VAE(corpus.vocab, config).to(device)
+    if config.warm_start:
+        logger.warning(
+            f"Warmstarting model from {config.warm_start} "
+            "This overrides all model configuration parameters. "
+        )
+        model_config = torch.load(os.path.join(config.warm_start, "config.pt"))
+        model_vocab = torch.load(os.path.join(config.warm_start, "vocab.pt"))
+        for k, v in corpus.vocab.c2i.items():
+            if model_vocab.c2i.get(k) != v:
+                raise ValueError(
+                    f"Vocab mismatch - NEW: {k}: {v}; OLD: {model_vocab.c2i.get(k)}"
+                )
+    else:
+        model_config = deepcopy(config)
+        model_vocab = deepcopy(corpus.vocab)
+
+    model = VAE(model_vocab, model_config).to(device)
+    if config.warm_start:
+        model.load(
+            path=os.path.join(config.warm_start, "model.pt"), map_location=config.device
+        )
+        logger.warning(f"Loaded model from {config.warm_start}")
 
     trainer = VAETrainer(config)
 
     torch.save(config, config.config_save)
-    torch.save(corpus.vocab, config.vocab_save)
+    torch.save(model_vocab, config.vocab_save)
     trainer.fit(model, train)
 
 
